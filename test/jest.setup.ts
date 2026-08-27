32454fd5d1bbb17ede7c42bd569913a999f14e27
David Morris <David.Morris@technyphion.com>
2026-08-27 19:00:08 +0200

    test: provide Web Crypto in Jest setup

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,7 +1,16 @@
 import { BrowserPlatform } from '@aurelia/platform-browser';
 import { setPlatform } from '@aurelia/testing';
 
+declare const require: (moduleName: 'crypto') => { webcrypto: Crypto };
+
 function bootstrapTestEnvironment() {
+    if (!globalThis.crypto?.subtle) {
+        Object.defineProperty(globalThis, 'crypto', {
+            configurable: true,
+            value: require('crypto').webcrypto,
+        });
+    }
+
     const platform = new BrowserPlatform(window);
     setPlatform(platform);
     BrowserPlatform.set(globalThis, platform);
